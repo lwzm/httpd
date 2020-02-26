8f0e1944c34468d100e7e0a7df2cf680f5bc948b
lwzm <lwzm@qq.com>
2020-02-26 12:51:59 +0800

    optimize stream

--- stream/main.go
+++ stream/main.go
@@ -4,45 +4,56 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/lwzm/httpd"
 )
 
-func init() {
-	channels := make(map[string](chan chan io.Reader))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		key := r.URL.RequestURI()
-		ctx := r.Context()
-		ch, ok := channels[key]
-		if !ok {
-			ch = make(chan chan io.Reader)
-			channels[key] = ch
-		}
-		if r.Method == "GET" {
-			select {
-			case chTmp := <-ch:
-				body := <-chTmp
-				n, err := io.Copy(w, body)
-				if err != nil {
-					log.Println("written", n, err)
-				}
-				close(chTmp)
-			case <-ctx.Done():
-				log.Println(ctx)
+var channels = make(map[string](chan chan io.Reader))
+var mutex = sync.Mutex{}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.RequestURI()
+	ctx := r.Context()
+	mutex.Lock()
+	ch, ok := channels[key]
+	if !ok {
+		ch = make(chan chan io.Reader)
+		channels[key] = ch
+	}
+	mutex.Unlock()
+	if r.Method == "GET" {
+		select {
+		case chTmp := <-ch:
+			body := <-chTmp
+			n, err := io.Copy(w, body)
+			if err != nil {
+				log.Printf("written %v, %v", n, err)
 			}
-		} else {
-			chTmp := make(chan io.Reader)
-			select {
-			case ch <- chTmp:
-				chTmp <- r.Body
-				<-chTmp
-			default:
-				w.WriteHeader(404)
-			}
+			close(chTmp)
+		case <-ctx.Done():
+			log.Print(ctx)
 		}
-	})
+	} else {
+		chTmp := make(chan io.Reader)
+		select {
+		case ch <- chTmp:
+			chTmp <- r.Body
+			// if flusher, ok := w.(http.Flusher); ok {
+			// 	flusher.Flush()
+			// }
+			<-chTmp
+		default:
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}
 }
 
 func main() {
+	http.HandleFunc("/", handler)
 	httpd.Start()
+	/*
+		curl -v -H Expect: --limit-rate 10k -T /dev/zero localhost:1111/z
+		curl -v localhost:1111/z >/dev/null
+	*/
 }
